81f06c702522e3973d70db85452892f3ca3f399d
ywl <ywlcjl2@163.com>
2015-12-21 19:22:18 +0800

    add tips ie10

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -9,7 +9,8 @@
             <p>&nbsp;</p>
             <p style="color: darkred;">安装说明: 新建一个数据库,导入cicreator.sql,并修改application/config/database.php的相应数据库设置.</p>
             <p>&nbsp;</p>
-            
+            <p style="color: darkkhaki;">注意: IE浏览器版本低于IE10, 文章本地图片上传功能可能无法使用.</p>
+            <p>&nbsp;</p>
             <p><a href="/welcome">CodeIgniter欢迎页</a></p>
             <p>更多CodeIgniter的内容您可以访问 <a href="http://www.codeigniter.com/" target="_blank">CodeIgniter官网</a>, &nbsp;&nbsp;<a href="http://codeigniter.org.cn/" target="_blank">CodeIgniter中国</a>.</p>
             <p>更多Bootstrap的内容您可以访问 <a href="http://getbootstrap.com/" target="_blank">Bootstrap官网</a>, &nbsp;&nbsp;<a href="http://www.bootcss.com/" target="_blank">Bootstrap中文网</a>.</p>
