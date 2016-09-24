814b663733be36392a6067d808515464002b73a5
ywl <ywlcjl2@163.com>
2016-09-24 12:20:44 +0800

    fix license

--- application/views/backend/home/index.php
+++ application/views/backend/home/index.php
@@ -6,7 +6,7 @@
         <p>Time <?php echo date('Y-m-d H:i:s'); ?>, 时区 <?php echo  date_default_timezone_get(); ?></p>
         <p>Support: <a href="https://github.com/ywlcjl/cicreator" target="_blank">https://github.com/ywlcjl/cicreator</a></p>
         <p>License : </p>
-        <p>Codeigniter 2.2.5 LICENSE</p>
+        <p>The MIT License (MIT)</p>
         <p>and GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 (GPL)</p>
         <p></p>
     </div>
